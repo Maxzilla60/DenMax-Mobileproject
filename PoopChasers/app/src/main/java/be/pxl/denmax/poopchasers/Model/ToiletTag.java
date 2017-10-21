e16f0000f5e7ec5ea2b0af478e97c4c1d52b5e45
Dennis Heperol <dennisheperol@gmail.com>
2017-10-21 22:49:39 +0200

    Spelling

--- PoopChasers/app/src/main/java/be/pxl/denmax/poopchasers/Model/ToiletTag.java
+++ PoopChasers/app/src/main/java/be/pxl/denmax/poopchasers/Model/ToiletTag.java
@@ -2,5 +2,5 @@ package be.pxl.denmax.poopchasers.Model;
 
 // A toiletlocation can be tagged with the following:
 public enum ToiletTag {
-    MENS, WOMENS, BABIES, FREE, ACCESSABLE, UNISEX
+    MENS, WOMENS, BABIES, FREE, ACCESSIBLE, UNISEX
 }
